1a9918fcdacd49d74eddd12392d47bc144cae86b
Adham <muzaffarov.adham@gmail.com>
2021-11-28 11:53:06 +0500

    using ts in express

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello2 from typescript");
+import express from "express";
+const app = express();
+
+interface Params {
+    a: number;
+    b?: number;
+}
+
+const add = (x: Params): number => {
+    return x.b ? x.a + x.b : x.a;
+};
+
+app.get("/", (req, res) => {
+    (req as any).name = "Adham";
+    res.send("OK");
+});
+
+app.get("/add", (req: any, res) => {
+    req.name = "Adham";
+    const result = add({ a: 1, b: 2 });
+    res.send(result + "");
+});
+
+app.listen(4000, () => console.log("Server is running..."));
