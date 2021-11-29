b511ca0f11422ab59caed3d9c6c54a52c2bf3752
Adham <muzaffarov.adham@gmail.com>
2021-11-29 06:39:51 +0500

    basic concepts

---
+++ starter.ts
@@ -0,0 +1,66 @@
+// let firstName = "Adham";
+// console.log(firstName);
+
+// interface User {
+//     firstName: string;
+//     age: number;
+//     isSingle: boolean;
+// }
+
+// const user: User = {
+//     firstName: "Adham",
+//     age: 21,
+//     isSingle: true,
+// };
+
+// const getUser = (): User => {
+//     return {
+//         firstName: "Adham",
+//         age: 21,
+//         isSingle: true,
+//     };
+// };
+
+// console.log(getUser());
+
+// let userId: 1 | 2 | 3 | 4;
+// userId = 4;
+// console.log(userId);
+
+// let lastName: string | undefined;
+// console.log(lastName);
+
+// type age = number | string;
+// let Myage: age;
+// Myage = 21;
+// console.log(Myage);
+
+// function getAge(age: age) {
+//     return `my age is ${age}`;
+// }
+
+// console.log(getAge(21));
+
+// type Color = Array<string>;
+// let colorName: Color;
+// colorName = ["red", "blue", "black", "white", "green", 1];
+// console.log(colorName);
+
+// type Users = Array<{ name: string; age: number }>;
+
+// let users: Users = [{ name: "Adham", age: 21 }];
+
+// console.log(users);
+
+// class User {
+//     firstName: string;
+//     age: number;
+
+//     constructor(firstName: string, age: number) {
+//         this.firstName = firstName;
+//         this.age = age;
+//     }
+// }
+
+// const Adham = new User("Adham", 21);
+// console.log(Adham);
